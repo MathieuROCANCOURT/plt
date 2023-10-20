115609b1bdd1b886871c5a4e38a52ed6be2e888c
MathieuROCANCOURT <rocancourt.mathieu@ensea.fr>
2023-10-20 16:46:10 +0200

    Modify state.dia and test_shared_dummy.cpp.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -15,13 +15,16 @@ using namespace std;
 using namespace state;
 
 int main(int argc, char *argv[]) {
-    Exemple exemple;
-    exemple.setX(53);
+    if (argc == 2) {
+        if (strcmp(argv[1], "hello") == 0) {
+            cout << "Bonjour le monde!" << endl;
+        } else if (strcmp(argv[1], "state") == 0) {
+            cout << R"(Veillez utiliser les commandes "make unittest" et "make code-coverage" depuis le dossier build)"
+                 << endl;
+        } else {
+            cout << "It works !" << endl;
+        }
 
-    if (argc == 2 and strcmp(argv[1], "hello") == 0) {
-        cout << "Bonjour le monde!" << endl;
-    } else {
-        cout << "It works !" << endl;
     }
     return 0;
 }
