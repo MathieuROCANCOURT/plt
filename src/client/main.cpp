ccd87b7348472fbbc2455a2ca974781a4bf1e7c4
MargotLaleu <margot.laleu@ensea.fr>
2023-11-15 20:56:21 +0100

    dia2code fonctionnel : correction d'erreurs du state.dia et ajout du main.cpp

---
+++ src/client/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+#include <SFML/Graphics.hpp>
+
+
+#include <state.h>
+
+#include <cstring>
+
+using namespace std;
+using namespace state;
+
+
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        if (strcmp(argv[1], "hello") == 0) {
+            cout << "Bonjour le monde!" << endl;
+        } else if (strcmp(argv[1], "state") == 0) {
+            cout << R"(Veillez utiliser les commandes "make unittest" et "make code-coverage" depuis le dossier build)"
+                 << endl;
+        }
+    } else {
+        cout << "It works !" << endl;
+    }
+
+    return EXIT_SUCCESS;
+}
