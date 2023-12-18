4ee5272bc412ab18d9c9eac56d3abd15bbf69659
alexisR77 <alexis.rondeau.pro77@gmail.com>
2023-12-18 12:03:25 +0100

    buyApartCommand casi fini en attente de getter et setter nbAppart

--- src/shared/engine/buyApartCommand.cpp
+++ src/shared/engine/buyApartCommand.cpp
@@ -6,7 +6,7 @@
 
 
 void engine::buyApartCommand::execute (state::State& state){
-
+    buyApart(state);
 }
 
 
@@ -16,17 +16,55 @@ bool engine::buyApartCommand::buyApart(state::State &state) {
     std::vector<state::Property> myproperties=player->getPlayerProperties();
     int minpos=9999999;
     std::vector<state::Property> propertySameColor;
-    for (int i=0;i<myproperties.size();i++) {
+    std::vector<state::City> listville;
+    for (int i=0;i<myproperties.size();i++) {//met toute les propriété ayant la même couleur dans un vecteur à part
         if (myproperties[i].getColor()== this->getColor()) {
             propertySameColor.push_back(myproperties[i]);
         }
     }
-    for (int i = 0; i < propertySameColor.size(); i++) {
+    for (int i = 0; i < propertySameColor.size(); i++) {//transforme les propriétés en ville
         state::City* city =dynamic_cast<state::City*>(&propertySameColor[i]);
-        city->getNbApart();
+        listville.push_back(*city);
+    }
+    int a[listville.size()];
+    for (int i = 0; i < listville.size(); i++) {
+        a[i]=listville[i].getNbApart();
+    }
+    int b=a[listville.size()-1];
+    int nbMemenbApart=0;
+    for (int i = 0; i <listville.size(); i++) {//compte le nombre de ville ayant le même nombre d'appartement
+        if(b==a[i]){
+            nbMemenbApart++;
+        }
+    }
 
+    state::City cityApartAchetable=listville[listville.size()-1];
+    if (nbMemenbApart==listville.size()){//si toutes les ville ont le même nombre d'appartement choisi la ville ayant la position la plus faible
+        for (int i = 0; i < listville.size(); i++) {
+            if (listville[i].getPosition() < cityApartAchetable.getPosition()){
+                cityApartAchetable=listville[i];
+            }
+        }
+    }
+    std::vector<state::City> cityLessApart;
+    if (nbMemenbApart<listville.size()){
+        for (int i = 0; i < listville.size(); i++) {//si pas toutes les villes ont le même nombre d'appartement choisi les ville ayant le moins d'appartement
+            if (listville[i].getNbApart()<=cityApartAchetable.getNbApart()){
+                cityLessApart.push_back(listville[i]);
+            }
+        }
+        for (int i = 0; i < cityLessApart.size(); i++) {//choisi la ville parmi celle qui ont le moins d'apârtement la ville avec la position la moins élevé
+            if (cityApartAchetable.getPosition() > cityLessApart[i].getPosition()){
+                cityApartAchetable=cityLessApart[i];
+            }
+        }
 
     }
+    state::Player*playerAchetant;
+    //payTheBank(state,cityApartAchetable,cityApartAchetable);
+    playerAchetant=state.getCurrentPlayer();
+    //getter et setter NbAppart à ajouter + augmenter le nombre appart dans la ville acheté
+
 
     return false;
 }
