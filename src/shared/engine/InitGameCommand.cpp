1fcf2652820a08feb19f8cbc8cfdbf95bec43fb5
MargotLaleu <margot.laleu@ensea.fr>
2023-12-19 14:26:03 +0100

    bug

--- src/shared/engine/InitGameCommand.cpp
+++ src/shared/engine/InitGameCommand.cpp
@@ -7,16 +7,15 @@
 
 void engine::InitGameCommand::initPlayerInGame(state::State &state) {
 
+    std::string names[] = {"A", "B", "C", "D", "E", "F"};
+
     if(nbPlayerToInit > 1 and nbPlayerToInit < 7){
         state.modifyNbPlayer(nbPlayerToInit);
 
-        /*for(int i = 0; i < nbPlayerToInit ; i++){
-
-        }*/
-
-
-
-
+        for(int i = 0; i < nbPlayerToInit ; i++){
+            state::Player* newPlayer = new state::Player(names[i], (state::Token) i);
+            state.addPlayer(*newPlayer);
+        }
     }
 
 }
