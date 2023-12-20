aaa09c1e851633cb366bd156947523d12fa99428
MargotLaleu <margot.laleu@ensea.fr>
2023-12-20 17:31:00 +0100

    Création et début programmation BuyPropertyCommand.cpp

---
+++ src/shared/engine/BuyPropertyCommand.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ensea on 20/12/23.
+//
+
+#include "BuyPropertyCommand.h"
+
+
+void engine::BuyPropertyCommand::buyTheProperty(state::State &state) {
+    state::Player* playerCurrent = state.getCurrentPlayer();
+    //Achat de property (argument de la commande)
+}
+
+void engine::BuyPropertyCommand::execute(state::State &state) {
+    buyTheProperty(state);
+}
