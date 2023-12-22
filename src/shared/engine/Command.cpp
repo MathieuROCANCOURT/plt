1f5b50cbe01d6cb7df226f64a34d447018a95ac3
alexisR77 <alexis.rondeau.pro77@gmail.com>
2023-12-22 15:28:08 +0100

    modif nom

--- src/shared/engine/Command.cpp
+++ src/shared/engine/Command.cpp
@@ -1,1 +1,9 @@
+#include "Command.h"
 
+const engine::CommandTypeId engine::Command::getTypeId() {
+    return this->CommandId;
+}
+
+engine::Command::Command(engine::CommandTypeId TypeID) {
+    this->CommandId=TypeID;
+}
