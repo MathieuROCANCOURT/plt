748c0a6097ccf3d5507117d7a662266972eb0d65
alexisR77 <alexis.rondeau.pro77@gmail.com>
2023-12-22 10:42:47 +0100

    ajout d'un fichier de test

---
+++ test/shared/test_engine.cpp
@@ -0,0 +1,13 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "../../src/shared/engine.h"
+#include "../../src/shared/state.h"
+
+using namespace ::engine;
+
+BOOST_AUTO_TEST_CASE(Test_command){
+
+        BOOST_CHECK(1);
+
+}
