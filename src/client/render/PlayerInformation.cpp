7b3da6515d1c13aa9bda0110bb37963c6d8cfa69
MathieuROCANCOURT <mathieu.rocancourt@ensea.fr>
2024-01-03 17:55:46 +0100

    Add token when click player button

--- src/client/render/PlayerInformation.cpp
+++ src/client/render/PlayerInformation.cpp
@@ -5,20 +5,21 @@ using namespace std;
 using namespace render;
 
 PlayerInformation::PlayerInformation(vector<state::Player> listPlayer, sf::Vector2u sizeBoard) : listPlayer(
-        std::move(listPlayer)) {
+        std::move(listPlayer)), sizeBoard(sizeBoard) {
     int i = 0;
     for (auto player: this->listPlayer) {
-        auto *button = new Button(95, 40, float(sizeBoard.x) + float(i) * 110 + 10, 5, player.getName(),
-                                    float(sizeBoard.x) + float(i) * 110 + 20, 10);
-        this->dictPlayer.emplace(button, player.getMoney());
+        auto *button = new Button(95, 40, float(this->sizeBoard.x) + float(i) * 110 + 10, 5, player.getName(),
+                                  float(this->sizeBoard.x) + float(i) * 110 + 20, 10);
+        this->dictPlayer.emplace(button, player);
         this->listButtonPlayer->addButton(button);
         i++;
     }
-    this->listCasesPlayer = new AllCases(sizeBoard, uint(sizeBoard.y * 0.15));
-    this->textMoney = new Text("", float(sizeBoard.x * 1.1), float(sizeBoard.y * 0.09), 25, sf::Color(0x288330FF));
+    this->listCasesPlayer = new AllCases(this->sizeBoard, uint(this->sizeBoard.y * 0.15));
+    this->textMoney = new Text("", float(this->sizeBoard.x * 1.1), float(this->sizeBoard.y * 0.09), 25,
+                               sf::Color(0x288330FF));
 }
 
-std::map<Button *, long long> PlayerInformation::getDictPlayer() {
+std::map<Button *, state::Player> PlayerInformation::getDictPlayer() {
     return this->dictPlayer;
 }
 
@@ -27,7 +28,10 @@ void PlayerInformation::draw(sf::RenderWindow &window, sf::Vector2i cursorPos, s
     Button *test = this->listButtonPlayer->getFocus();
     if (test != nullptr) {
         this->listCasesPlayer->draw(window, cursorPos);
-        this->textMoney->setStringText(to_string(this->dictPlayer.at(test)));
+        this->textMoney->setStringText(to_string(this->dictPlayer.at(test).getMoney()));
+        Token showToken = *new Token(this->dictPlayer.at(test).getToken());
+        showToken.posMove(sf::Vector2f(float(this->sizeBoard.x * 1.03), float(this->sizeBoard.y * 0.09)));
+        showToken.draw(window);
         window.draw(this->textMoney->getText());
     }
     this->listButtonPlayer->draw(window, cursorPos, event);
